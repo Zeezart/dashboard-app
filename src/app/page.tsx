db625cc5319b93ac5e785f4e5b1efc88d6b53882
dzaky-pr <dzaky.pr.mulyono97@gmail.com>
2024-12-26 02:09:44 +0700

    feat: first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,50 @@
-import Homepage from '@/components/Homepage';
+'use client';
 
-import { getApiResponse } from '@/utils/shared/get-api-response';
+import { PaletteMode } from '@mui/material';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from 'react';
 
-import { NpmData, PageParams } from '@/types';
+import AppAppBar from '@/components/AppAppBar';
+import FAQ from '@/components/FAQ';
+import Features from '@/components/Features';
+import Footer from '@/components/Footer';
+import Hero from '@/components/Hero';
+import Highlights from '@/components/Highlights';
+import LogoCollection from '@/components/LogoCollection';
+import Pricing from '@/components/Pricing';
+import Testimonials from '@/components/Testimonials';
 
-const loadDataFromApi = async (slug?: string) => {
-  if (slug === 'testError500') {
-    throw new Error('This is mock a ssr 500 test error');
-  }
+export default function LandingPage() {
+  const [mode, setMode] = React.useState<PaletteMode>('light');
 
-  // Fetch & cache data from 2 remote APIs test
-  const [reactNpmData, nextJsNpmData] = await Promise.all([
-    getApiResponse<NpmData>({
-      apiEndpoint: 'https://registry.npmjs.org/react/latest',
-      revalidate: 60 * 60 * 24, // 24 hours cache
-      timeout: 5000, // 5 seconds
-    }),
-    getApiResponse<NpmData>({
-      apiEndpoint: 'https://registry.npmjs.org/next/latest',
-      revalidate: 0, // no cache
-      timeout: 5000, // 5 seconds
-    }),
-  ]);
+  const defaultTheme = createTheme({ palette: { mode } });
 
-  return {
-    reactNpmData,
-    nextJsNpmData,
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
   };
-};
-
-const AppHome = async ({ searchParams }: PageParams) => {
-  const slug = searchParams?.slug;
-  const { reactNpmData, nextJsNpmData } = await loadDataFromApi(slug);
 
   return (
-    <Homepage
-      reactVersion={reactNpmData?.version}
-      nextJsVersion={nextJsNpmData?.version}
-    />
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+      <Hero />
+      <Box sx={{ bgcolor: 'background.default' }}>
+        <LogoCollection />
+        <Features />
+        <Divider />
+        <Testimonials />
+        <Divider />
+        <Highlights />
+        <Divider />
+        <Pricing />
+        <Divider />
+        <FAQ />
+        <Divider />
+        <Footer />
+      </Box>
+    </ThemeProvider>
   );
-};
-
-export default AppHome;
+}
